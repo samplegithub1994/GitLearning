ecd3f8895a9f901cb22a7103648610d36c44a8a8
Ashok Panda <pandapravas94@gmail.com>
2023-04-14 02:26:04 +0530

    New branch added

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('do not worry i am good')
+    name='Try2Catch'
+    print_hi(f'do not worry i am good.:{name}')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
